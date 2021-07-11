77a29bfa8119f5a944af19ede0a48ac9dbb1ed76
himani412 <77782996+himani412@users.noreply.github.com>
2021-07-11 08:04:31 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.hide();
-  classifier=ml5.imageClassifier('MobileNet',modelLoaded);
-}
-function modelLoaded(){
-  console.log('ModelLoaded!');
-}
-function draw(){
-  image(video,0,0,300,300);
-  classifier.classify(video,gotresult);
-}
-function gotresult(error,results){
-  if(error){
-    console.error(error);
-  }
-  else{
-    console.log(results);
-    document.getElementById('resultobjectname').innerHTML=results[0].label;
-document.getElementById('resultobjectaccuracy').innerHTML=results[0].confidence.toFixed(3);
-  }
+function setup(){
+    video=createCapture(VIDEO);
+video.size(550,500);
+
+canvas=createCanvas(550,550);
+canvas.position(560,150);
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotposes);
 }
 
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+function modelLoaded(){
+    console.log('ModelLoaded!');
+}
+function draw(){
+    background('#990066');
+}
